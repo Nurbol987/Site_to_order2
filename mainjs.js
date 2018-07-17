1aee25803496a780a6a314a19d26eb59b66c3ac0
Nurbol987 <39963526+Nurbol987@users.noreply.github.com>
2018-07-18 02:20:58 +0600

    Add files via upload

---
+++ mainjs.js
@@ -0,0 +1,8 @@
+ jQuery('document').ready(function(){
+alert('Проводите мишку по 3-ом началном картинкам');
+alert('Если Вы на телефоне нажмите по 3-ом началном картинки');
+var zak1=("Полный проект");
+jQuery('button').on('click', function(){
+	alert("Вы заказали" + zak1);
+})
+ })
